3817f8c4bd219f349167283b9919ffdc41c37d3f
fam246 <ola.fam10@gmail.com>
2017-06-01 22:57:21 +0100

    Post Model

--- app/Comments.php
+++ app/Comments.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Comments extends Model
 {
     //
+    protected $guarded = ['id'];
+    public function author()
+    {
+        return $this->belongsTo('App/User','with_user');
+    }
+    public function post()
+    {
+        return $this->belongsTo('App/Posts','with_post');
+    }
+
 }
