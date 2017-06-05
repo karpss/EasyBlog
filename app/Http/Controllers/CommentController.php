8e58da89baf09a5a7c35b38a757b0e23f7177878
fam246 <ola.fam10@gmail.com>
2017-06-05 23:14:58 +0100

    Comment controller

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Comments;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -9,4 +10,14 @@ use App\Http\Requests;
 class CommentController extends Controller
 {
     //
+    public function store(Request $request)
+    {
+        //on_post, from_user, body
+        $input['from_user'] = $request->user()->id;
+        $input['on_post'] = $request->input('on_post');
+        $input['body'] = $request->input('body');
+        $slug = $request->input('slug');
+        Comments::create( $input );
+        return redirect($slug)->with('message', 'Comment published');
+    }
 }
