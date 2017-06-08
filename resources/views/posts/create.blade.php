3b56a5ec4ffa06ba5046e1608de5dc5dbb32a797
fam246 <ola.fam10@gmail.com>
2017-06-08 15:21:36 +0100

    tinyMCE Added

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,6 +3,14 @@
     Add New Post
 @endsection
 @section('content')
+    <script type="text/javascript" src="{{ asset('/js/tinymce/tinymce.min.js') }}"></script>
+    <script type="text/javascript">
+        tinymce.init({
+            selector : "textarea",
+            plugins : ["advlist autolink lists link image charmap print preview anchor", "searchreplace visualblocks code fullscreen", "insertdatetime media table contextmenu paste"],
+            toolbar : "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+        });
+    </script>
     <form action="/new-post" method="post">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <div class="form-group">
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,6 +3,14 @@
     Edit Post
 @endsection
 @section('content')
+    <script type="text/javascript" src="{{ asset('/js/tinymce/tinymce.min.js') }}"></script>
+    <script type="text/javascript">
+        tinymce.init({
+            selector : "textarea",
+            plugins : ["advlist autolink lists link image charmap print preview anchor", "searchreplace visualblocks code fullscreen", "insertdatetime media table contextmenu paste"],
+            toolbar : "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+        });
+    </script>
     <form method="post" action='{{ url("/update") }}'>
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <input type="hidden" name="post_id" value="{{ $post->id }}{{ old('post_id') }}">
