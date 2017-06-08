3ccc98c46aa829cbe550dbb170fccdec2abcfbba
fam246 <ola.fam10@gmail.com>
2017-06-08 15:27:28 +0100

    User Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,5 +8,66 @@ use App\Http\Requests;
 
 class UserController extends Controller
 {
-    //
+    /*
+   * Display active posts of a particular user
+   *
+   * @param int $id
+   * @return view
+   */
+    public function user_posts($id)
+    {
+        //
+        $posts = Posts::where('author_id',$id)->where('active',1)->orderBy('created_at','desc')->paginate(5);
+        $title = User::find($id)->name;
+        return view('home')->withPosts($posts)->withTitle($title);
+    }
+    /*
+     * Display all of the posts of a particular user
+     *
+     * @param Request $request
+     * @return view
+     */
+    public function user_posts_all(Request $request)
+    {
+        //
+        $user = $request->user();
+        $posts = Posts::where('author_id',$user->id)->orderBy('created_at','desc')->paginate(5);
+        $title = $user->name;
+        return view('home')->withPosts($posts)->withTitle($title);
+    }
+    /*
+     * Display draft posts of a currently active user
+     *
+     * @param Request $request
+     * @return view
+     */
+    public function user_posts_draft(Request $request)
+    {
+        //
+        $user = $request->user();
+        $posts = Posts::where('author_id',$user->id)->where('active',0)->orderBy('created_at','desc')->paginate(5);
+        $title = $user->name;
+        return view('home')->withPosts($posts)->withTitle($title);
+    }
+    /**
+     * profile for user
+     */
+    public function profile(Request $request, $id)
+    {
+        $data['user'] = User::find($id);
+        if (!$data['user'])
+            return redirect('/');
+        if ($request -> user() && $data['user'] -> id == $request -> user() -> id) {
+            $data['author'] = true;
+        } else {
+            $data['author'] = null;
+        }
+        $data['comments_count'] = $data['user'] -> comments -> count();
+        $data['posts_count'] = $data['user'] -> posts -> count();
+        $data['posts_active_count'] = $data['user'] -> posts -> where('active', '1') -> count();
+        $data['posts_draft_count'] = $data['posts_count'] - $data['posts_active_count'];
+        $data['latest_posts'] = $data['user'] -> posts -> where('active', '1') -> take(5);
+        $data['latest_comments'] = $data['user'] -> comments -> take(5);
+        return view('admin.profile', $data);
+    }
 }
